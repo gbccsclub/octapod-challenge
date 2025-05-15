7e026015ba593d6593b162ef1bc3b8ad662ef4a0
Sokmontrey <sythatsokmontrey@gmail.com>
2025-05-14 22:12:02 -0400

    fix: 60 seconds again

--- internal/lobby.go
+++ internal/lobby.go
@@ -14,7 +14,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var UpdateInterval = 1 * 2 * time.Second
+var UpdateInterval = 1 * 60 * time.Second
 var TimeoutInterval = 2 * time.Second
 var MaxInactive = 2
 
